6659b0ab1406e1c5e2eae51a732f1e20a53de40a
Mahdi Hasanpour <mh.hasnapour@gmail.com>
2022-02-24 18:18:07 +0330

    test change

--- 5. Longest Palindromic Substring/soloution.php
+++ 5. Longest Palindromic Substring/soloution.php
@@ -1,12 +1,14 @@
 <?php
 
-class Solution {
+class Solution
+{
 
     /**
      * @param String $s
      * @return String
      */
-    function longestPalindrome($s) {
+    function longestPalindrome($s)
+    {
         $len = strlen($s);
         if ($len === 1)
             return $s;
@@ -27,15 +29,21 @@ class Solution {
 
 system('cls');
 $s = new Solution();
-$t = time();
-/*echo $s->longestPalindrome("badasdfasdfasdfmadamsadf") . PHP_EOL;
-echo $s->longestPalindrome("cbbd") . PHP_EOL;
-echo $s->longestPalindrome("aa") . PHP_EOL;
-echo $s->longestPalindrome("ab") . PHP_EOL;
-echo $s->longestPalindrome("a") . PHP_EOL;
-echo $s->longestPalindrome("lphbehiapswjudnbcossedgioawodnwdruaaxhbkwrxyzaxygmnjgwysafuqbmtzwdkihbwkrjefrsgjbrycembzzlwhxneiijgzidhngbmxwkhphoctpilgooitqbpjxhwrekiqupmlcvawaiposqttsdgzcsjqrmlgyvkkipfigttahljdhtksrozehkzgshekeaxezrswvtinyouomqybqsrtegwwqhqivgnyehpzrhgzckpnnpvajqevbzeksvbezoqygjtdouecnhpjibmsgmcqcgxwzlzztdneahixxhwwuehfsiqghgeunpxgvavqbqrelnvhnnyqnjqfysfltclzeoaletjfzskzvcdwhlbmwbdkxnyqappjzwlowslwcbbmcxoiqkjaqqwvkybimebapkorhfdzntodhpbhgmsspgkbetmgkqlolsltpulgsmyapgjeswazvhxedqsypejwuzlvegtusjcsoenrcmypexkjxyduohlvkhwbrtzjnarusbouwamazzejhnetfqbidalfomecehfmzqkhndpkxinzkpxvhwargbwvaeqbzdhxzmmeeozxxtzpylohvdwoqocvutcelgdsnmubyeeeufdaoznxpvdiwnkjliqtgcmvhilndcdelpnilszzerdcuokyhcxjuedjielvngarsgxkemvhlzuprywlypxeezaxoqfges") . PHP_EOL;*/
-echo $s->longestPalindrome("aacabdkacaa") . PHP_EOL;
-echo $s->longestPalindrome("aacabdkacaamadam") . PHP_EOL;
+$tc = [
+    "babad",
+    "cbbd",
+    "aacabdkacaa",
+    "aacabdkacaamadam",
+    "aacabdkacaaMAdamasdfasdfasfwer",
+    "uhrfjotnewtodhmbplsaolnpcdaohiytmfllukijouxipvqohtsgxbtfoxyfkfczkfwhzimbefiohmtimrcxbpgcxogystdkcqujvbxsgirpccdnvejtljftwkdpsqpflzwruwwdzovsbmwbcvlftkjnxqaguvtsycylqzquqkbnybnbaeahbxejhphwrpmymcemuhljwtuvxefqfzjhskuqhifydkxpnfwfxkpeexnjltfqwfvchphmtsrsyayxukvmlqodshqwbeaxhcxdbssnrdzvxtusngwqdxvluauphmmbwmgtazjwvolenegwbmjfwprfuswamyvgrgshqocnhirgyakbkkggviorawadzhjipjjgiwpelwxvtaegauerbwpalofrbghfhnublttqtcmqskcocwwwxpnckrnbepusjyohsrretrqyvgnbezuvwmzizcefxyumtdwnqjkgsktyuacfpnqocqjxcurmipjfqmjqrkdeqsfseyigqlwmzgqhivbqalcxhlzgtsfjbdbfqiedogrqasgmimifdexbjjpfusxsypxobxjtcwxnkpgkdpgskgkvezkriixpxkkattyplnpdbdifforxozfngmlgcunbnubzamgkkfbswuqfqrvzjqmlfqxeqpjaqayodtetsecmfbplscmslpqiyhhykftzkkhshxqvdwmwowokpluwyvavwvofwqtdilwqjgrprukzyhckuspyzaoe",
+    "asdfasifpwoeir239482u3498usdfoiajsdflakjfri2394MADAM8u23948usadfasdfsadfasff29348u23498sadfsadjifasdfji2302349sadfajsdfaksjdf23482394823492348928423948uoisaisdfjasdfu9asd9f8uasd09f8uasd9f8uasdf98sdf9s8adufasd98fusad90f8uasdf9u8asdf98asudf9sa8fuysad098fusadf8usd8uasdf8asudf9sa8dfusd9f87yuasdf98usdfu8asdf98u2349823y492834",
+    "aacabdkacaa",
+    "aacabdkacaamadam"
+];
+$t = microtime(true);
+
+foreach ($tc as $test)
+    echo $s->longestPalindrome($test) . PHP_EOL;
 
 
-echo "\n\n\n\nTime: " . (time() - $t);
+echo "\n\n\n\nTime: " . (microtime(true) - $t) * 1000;

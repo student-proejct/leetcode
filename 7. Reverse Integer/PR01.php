c495c5ba71633a622ef6a05a28c0c0aff0bffc16
Mahdi Hasanpour <mh.hasnapour@gmail.com>
2022-07-22 17:49:36 +0430

    correct response

--- 7. Reverse Integer/PR01.php
+++ 7. Reverse Integer/PR01.php
@@ -13,11 +13,15 @@ class Solution
             return '0';
         $is_negative = $x < 0;
         $str = strrev(preg_replace('/[^0-9]/', '', strval($x)));
-
+        $intval = ($is_negative ? -1 : 1) * intval($str);
+        if ($intval < -(2 ** 31) || $intval > (2 ** 31 - 1))
+            return '0';
         return ($is_negative ? '-' : '') . ltrim($str, '0');
     }
 }
 
 
-echo (new Solution())->reverse(210);
+echo (new Solution())->reverse(123) . "\n";
+echo (new Solution())->reverse(021) . "\n";
+echo (new Solution())->reverse(1534236469) . "\n";
 
